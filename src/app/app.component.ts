c3a0595ba1289b8b55159e1e7457c30f11aaf4bf
Shiyam <shiyams315@gmail.com>
2025-03-11 22:37:22 +0530

    v1 complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatFormFieldModule, FormsModule, MatInputModule, MatButtonModule, MatListModule, MatIconModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'todo_list';
+  task = '';
+  tasks: string[] = [];
+
+
+  addTask() {
+    if (this.task != '') {
+      this.tasks.push(this.task);
+      this.task = '';
+    }
+  }
+
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
